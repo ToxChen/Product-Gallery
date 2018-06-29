ee8a0c4e36cacb642415bc5a49a310cb29ed7087
JL Chen <azx011014@gmail.com>
2018-06-29 18:01:10 +0800

    Update 2018-06-29T10:01:05.094Z

--- javascripts/all.js
+++ javascripts/all.js
@@ -1,2 +1,12 @@
-"use strict";var helpers={getQuery:function(){return"getQuery"}};
+'use strict';
+
+console.log('return ' + helpers.getQuery());
+console.log('return ' + helpers.getQuery() + ' line 2');
+// ES6 範例程式碼
+var helpers = {
+  getQuery: function getQuery() {
+    console.log('getQuery');
+    return 'getQuery';
+  }
+};
 //# sourceMappingURL=all.js.map
